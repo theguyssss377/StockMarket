cd87bfbc380ab1fc8c9dd7a0ea48267525f512b8
theguyssss377 <theguyssss377@gmail.com>
2026-04-10 13:47:00 -0500

    yer

--- stockproject.py
+++ stockproject.py
@@ -1,11 +1,22 @@
-import alpaca_trade_api as trade api
-import pandas as pd
+import alpaca_trade_api as tradeapi
 import time
-API_KEY = 
-BASE_URL = 
+API_KEY = "PKCTPB3AWDM2UHCV7JATMHRAJB"
+API_SECRET = "HUr2U5YzBDg2zNC2BXk7vkB2XfzZGkKPfTDsstcnzsRo"
+BASE_URL = "https://paper-api.alpaca.markets"
 
 SYMBOL = "AAPL"
-SHORT_WINDOW = 
-LONG_WINDOW = 
-QTY = 50
+QTY = 1
 
+api = tradeapi.REST(API_KEY, API_SECRET, BASE_URL, api_version="v2")
+
+while True:
+    api.submit_order(
+symbol=SYMBOL,
+qty=QTY,
+side="buy",
+type="market",
+time_in_force="gtc"
+    )
+
+    print("Bought", QTY, SYMBOL)
+    time.sleep(28800)
